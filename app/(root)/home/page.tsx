28fe454447b6a29b9f63d3d5a0cf9810367d5983
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-09-22 07:53:51 +0545

    feat(metadata): added metadata example page

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export async function generateMetadata() {
+  return {
+    title: 'Home Page',
+    description: 'This is the home page description',
+  };
+}
+
+const page = () => {
+
+  return (
+    <div>hello this is page bro</div>
+  )
+}
+
+export default page
