4a94fd77096db54f265d5048b9555e6c848bc9a2
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-10-19 23:56:41 +0545

    basic dynamic routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main>
-   
+      <Link href="/project/list" className="mb-6 text-blue-600 underline">
+        project list
+      </Link>
       <h1>Hello, Next.js!</h1>
       <p>Welcome to your Next.js application.</p>
       
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,15 +5,17 @@ const styles = require('./Navbar.module.css')
 const Navbar = () => {
     return (
         <header className={styles.header}>
-            <nav className={styles.nav}>
+            <nav className="flex justify-between items-center mx-auto p-4 max-w-6xl underline-none">
                 <p className={styles.brand}>Next.js</p>
 
-                <ul className={styles.menu}>
+                <ul className= "flex space-x-6 list-none">
                     <Link href="/"><li className={styles.item}>Home</li></Link>
                     <Link href="/about"><li className={styles.item}>About</li></Link>
                     <Link href="/contact"><li className={styles.item}>Contact</li></Link>
                 </ul>
+             
             </nav>
+               <hr />
         </header>
     )
 }
