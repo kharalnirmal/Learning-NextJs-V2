b4d51d44eaafee2a75d28f306e441ab5bd9afae1
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-06 16:59:52 +0545

    catch all  where docs cant be found

---
+++ app/docs/[...slug]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = ({ params }: { params: { slug: string[] } }) => {
+  return <div>{params.slug}</div>;
+};
+
+export default page;
+
+// 🔖 Slug (Next.js)
+
+// A slug is a dynamic part of the URL that changes based on data.
+// In Next.js, slugs are created using square brackets in folder names.
+
+// app/
+//  └── blog/
+//      └── [slug]/
+//          └── page.tsx   // /blog/react, /blog/nextjs
+
+// [slug] captures the value from the URL and lets one page handle many routes.
