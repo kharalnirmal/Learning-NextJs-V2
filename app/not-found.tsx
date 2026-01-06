5a63b0e26b988a4c859bf4de17bc054276ed4b62
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-06 19:53:36 +0545

    not found page final

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-1 justify-center items-center gap-8">
+    <div className="flex flex-col justify-center items-center bg-white h-screen text-black">
+      <div className="flex justify-center items-center gap-8">
         <Image
           src={"/undraw_page-not-found_6wni.svg"}
           alt="Not-found Image"
@@ -15,6 +15,12 @@ export default function NotFound() {
         <span className="bg-gray-500 rounded-3xl w-[3px] h-30"></span>
         <h1 className="text-4xl">Not Found Sorry Bro</h1>
       </div>
+      <Link
+        href={"/"}
+        className="bg-teal-400 mt-3 px-5 py-2 rounded-4xl hover:scale-105 active:scale-95"
+      >
+        Return Home
+      </Link>
     </div>
   );
 }
