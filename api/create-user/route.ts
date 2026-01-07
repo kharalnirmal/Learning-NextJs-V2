a20b9794846ecd8358e0416ea91bbb19919fd404
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-07 13:53:41 +0545

    POST ENDPOINT CREATED

---
+++ api/create-user/route.ts
@@ -0,0 +1,62 @@
+import { error } from "console";
+import { NextResponse } from "next/server";
+type users = {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+};
+const users: users[] = [];
+
+export async function POST(request: Request) {
+  try {
+    console.log(request);
+    const { name, email, age } = await request.json();
+
+    if (!name || !email || !age) {
+      return NextResponse.json(
+        {
+          sucess: false,
+          error: "Name ,Email and age is must required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = users.find((user) => user.email === email);
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          sucess: false,
+          error: "users mail already exist try diff one",
+        },
+        { status: 400 }
+      );
+    }
+    // Create new user
+    const newUser: users = {
+      id: users.length + 1,
+      name,
+      email,
+      age,
+    };
+    users.push(newUser);
+
+    return NextResponse.json(
+      {
+        sucess: true,
+        date: users,
+        message: "user Created0",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        sucess: false,
+        error: "user not created",
+      },
+      { status: 500 }
+    );
+  }
+}
