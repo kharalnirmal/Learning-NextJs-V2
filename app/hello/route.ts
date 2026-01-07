b8ce5d152f5d18e812d36046e95c5c7999a0280c
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-07 12:32:20 +0545

    basic get route

---
+++ app/hello/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const users = [
+    {
+      id: 1,
+      name: "Aarav Sharma",
+      email: "aarav.sharma@example.com",
+      age: 24,
+      role: "Frontend Developer",
+    },
+    {
+      id: 2,
+      name: "Nisha Verma",
+      email: "nisha.verma@example.com",
+      age: 27,
+      role: "UI/UX Designer",
+    },
+    {
+      id: 3,
+      name: "Rohit Mehta",
+      email: "rohit.mehta@example.com",
+      age: 29,
+      role: "Backend Developer",
+    },
+    {
+      id: 4,
+      name: "Priya Kapoor",
+      email: "priya.kapoor@example.com",
+      age: 26,
+      role: "Product Manager",
+    },
+    {
+      id: 5,
+      name: "Kunal Singh",
+      email: "kunal.singh@example.com",
+      age: 31,
+      role: "DevOps Engineer",
+    },
+  ];
+
+  return NextResponse.json({
+    users,
+  });
+}
